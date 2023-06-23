984cd1da031a7fb9f266281ba9e39b7071b98a81
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-06-23 15:17:11 -0700

    scaffold player and computer move functions

--- tic_tac_toe_cli.py
+++ tic_tac_toe_cli.py
@@ -2,12 +2,12 @@ import random
 
 board = [[' ' for x in range(3)] for y in range(3)]
 
-for i in range(3):
-    for j in range(3):
-        board[i][j] = random.choice(['X', 'O'])
+# for i in range(3):
+#     for j in range(3):
+#         board[i][j] = random.choice(['X', 'O'])
+# computer_move = random.choice([0, 1])
 
-
-def create_board(game_board):
+def create_board():
     horizontal_line = "------------------"
     print(horizontal_line)
     for row in range(3):
@@ -17,4 +17,35 @@ def create_board(game_board):
         print("\n" + horizontal_line)
 
 
-create_board(board)
+create_board()
+
+
+def player_move(game_board, user_input):
+    if user_input == 'X' or user_input == 'O':
+        print("Player move")
+        row = int(input("Enter row: "))
+        col = int(input("Enter column: "))
+        if game_board[row][col] == ' ':
+            game_board[row][col] = user_input
+            create_board()
+            computer_move(game_board, 'O')
+        else:
+            print("This place is already taken. Try again")
+            player_move(game_board, user_input)
+    else:
+        print("Wrong input. Try again")
+        player_move(game_board, user_input)
+
+
+def computer_move(game_board, computer_input):
+    print("Computer move")
+    row = random.randint(0, 2)
+    col = random.randint(0, 2)
+    if game_board[row][col] == ' ':
+        game_board[row][col] = computer_input
+        create_board()
+    else:
+        computer_move(game_board, computer_input)
+
+
+player_move(board, 'X')
